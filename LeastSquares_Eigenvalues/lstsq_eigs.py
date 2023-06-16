af4158473706c13c4139e70eef2fe5096e208d17
Jordan <mccaslij@oregonstate.edu>
2023-06-16 06:44:46 -0700

    Trying to fix data import issue ifn LeastSquares file

--- LeastSquares_Eigenvalues/lstsq_eigs.py
+++ LeastSquares_Eigenvalues/lstsq_eigs.py
@@ -42,7 +42,7 @@ def line_fit():
     index for the data in housing.npy. Plot both the data points and the least
     squares line.
     """
-    data = np.load("LeastSquares_Eigenvalues/housing.npy")
+    data = np.load("housing.npy")
     year = data[:,0]
     n = len(year)
     price = data[:,1].reshape((n,1))
@@ -67,28 +67,62 @@ def polynomial_fit():
     the year to the housing price index for the data in housing.npy. Plot both
     the data points and the least squares polynomials in individual subplots.
     """
-    data = np.load("housing.npy")
+    data = np.load("LeastSquares_Eigenvalues\housing.npy")
     year = data[:,0]
     n = len(year)
     x = np.linspace(min(year),max(year), 100)
     price = data[:,1].reshape((n,1))
     plt.scatter(year,price, label = 'Data')
     A = np.vander(year,12)
-    for i in range(1,5):
-        n = i * 3
-        lab = "Polynomial fit of degree " + str(n)
-        An = A[:,12-n:12]
-        l = least_squares(An,price)
-        f = np.poly1d(l.transpose()[0])
-        y = f(x)
-        plt.plot(x,y, label = lab)
-    plt.legend()
+    lab = "Polynomial fit of degree " + str(3)
+    An = A[:,12-4:12]
+    l = least_squares(An,price)
+    f = np.poly1d(l.transpose()[0])
+    y = f(x)
+
+    ax1 = plt.subplot(2,2,1)
+    ax2 = plt.subplot(2,2,2)
+    ax3 = plt.subplot(2,2,3)
+    ax4 = plt.subplot(2,2,4)
+    ax1.plot(x,y, label = lab)
+    ax1.scatter(year,price,label = 'Data')
+    ax1.set_title('Degree 3 fit')
+
+    lab = "Polynomial fit of degree " + str(6)
+    An = A[:,12-7:12]
+    l = least_squares(An,price)
+    f = np.poly1d(l.transpose()[0])
+    y = f(x)
+    ax2.plot(x,y, label = lab)
+    ax2.scatter(year,price,label = 'Data')
+    ax2.set_title('Degree 6 fit')
+
+    lab = "Polynomial fit of degree " + str(9)
+    An = A[:,12-10:12]
+    l = least_squares(An,price)
+    f = np.poly1d(l.transpose()[0])
+    y = f(x)
+    ax3.plot(x,y, label = lab)
+    ax3.scatter(year,price,label = 'Data')
+    ax3.set_title('Degree 9 fit')
+
+    lab = "Polynomial fit of degree " + str(12)
+    An = A
+    l = least_squares(An,price)
+    f = np.poly1d(l.transpose()[0])
+    y = f(x)
+    ax4.plot(x,y, label = lab)
+    ax4.scatter(year,price,label = 'Data')
+    ax4.set_title('Degree 12 fit')
+
+
+
+    
     plt.xlabel("Years since 2000")
     plt.ylabel('Housing Price Index')
-    plt.title('Problem 3')
+    plt.suptitle('Problem 3')
     return plt.show()
 
-    raise NotImplementedError("Problem 3 Incomplete")
 polynomial_fit()
 
 def plot_ellipse(a, b, c, d, e):
